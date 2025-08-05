0018f0e27dac38fce0912a8cc8be5fefc070a3e0
Sabina Bernardes <66752747+sabinabernardes@users.noreply.github.com>
2025-08-05 11:35:51 -0300

    Remove obsolete Compose BOM version and update dependencies in Gradle configuration

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.compose.bom.v20240500)
+    implementation(libs.androidx.compose.bom)
     implementation(libs.androidx.ui.graphics.android)
     implementation(libs.androidx.ui.text.android)
     implementation(libs.androidx.material3.android)
