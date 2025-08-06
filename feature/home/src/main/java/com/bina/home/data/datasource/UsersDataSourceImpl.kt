d0b67b85bc7d325fe96a8b414e3bb5390b2aac38
Sabina Bernardes <66752747+sabinabernardes@users.noreply.github.com>
2025-08-06 14:31:44 -0300

    Update Room library versions and handle errors in UsersDataSourceImpl

--- feature/home/src/main/java/com/bina/home/data/datasource/UsersDataSourceImpl.kt
+++ feature/home/src/main/java/com/bina/home/data/datasource/UsersDataSourceImpl.kt
@@ -3,12 +3,15 @@ package com.bina.home.data.datasource
 import com.bina.home.data.model.User
 import com.bina.home.data.service.PicPayService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 
 class UsersDataSourceImpl(private val service: PicPayService) : UsersDataSource {
     override suspend fun getUsers(): Flow<List<User>> {
         return flow {
             emit(service.getUsers())
+        }.catch {
+            emit(emptyList())
         }
     }
 }
